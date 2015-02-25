66912e7a300ac0bfd481d8f8ddb97da7e5a14fa8
Wayne <dev.wayne.ix@gmail.com>
2015-02-25 10:24:05 +0800

    involve commanderjs for cli

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,7 +1,27 @@
 var watchify = require('watchify');
 var fromArgs = require('watchify/bin/args');
+var program = require('commander');
+var browserify =require('browserify');
+
+var pkg = require('../lib/modules');
+
+//var b = browserify(watchify.args);
+
+//var w = watchify(b,{});
+
+program
+    .version('0.0.1')
+    .option('-a, --all', 'Package all js and css files')
+    .option('-j, --js', 'Package js files')
+    .option('-c, --css', 'Package css files')
+    .parse(process.argv);
 
 
-var b = browserify(watchify.args);
-
-var w = watchify(b,{});
+if (program.css) {
+    pkg.pkgCss();
+} else if (program.js) {
+    pkg.pkgJs();
+} else {
+    pkg.pkgCss();
+    pkg.pkgJs();
+}
